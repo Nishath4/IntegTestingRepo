2163393bbfab133bd58d429d6164ca4457345caf
nishath.n <nishath.n@zohocorp.com>
2019-01-22 23:29:33 +0530

    functionsChange

--- functions/Test1.java
+++ functions/Test1.java
@@ -1,10 +1,13 @@
 import com.zoho.cloud.function.Context;
 import com.zoho.cloud.function.basic.*;
 import java.util.*;
+import org.json.simple.JSONObject;
 public class Test1 implements ZCFunction
 {
 	public void runner(Context context, BasicIO basicIO) throws Exception
 	{
-		basicIO.write( "Test 1 Running" );
+		JSONObject json = new JSONObject();
+		json.put("Nishath","Test1");
+		basicIO.write( "Test 1 Running"+json.toJSONString());
 	}
 }
